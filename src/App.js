b683229449aa00e3f21ca9e76392da9e27b37985
Arthur Freitas <arthur.freitas@compasso.com.br>
2021-03-08 09:40:17 -0300

    feat(init): add basic styling and markup

--- src/App.js
+++ src/App.js
@@ -1,27 +1,43 @@
+import FilterButton from "./components/FilterButton";
+import Form from "./components/Form";
+import Todo from "./components/Todo";
+
 function App(props) {
+  function addTask(name) {
+    alert(name);
+  }
+
+  const taskList = props.tasks.map(task => (
+    <Todo
+      id={task.id}
+      name={task.name}
+      completed={task.completed}
+      key={task.id}
+    />
+  ));
+
   return (
     <div className="todoapp stack-large">
-      <h1>To-Do-matic</h1>
+      <h1>To-Do-Matic</h1>
 
-      <form>
-        <h2 className="label-wrapper">
-          <label htmlFor="new-todo-input" className="label__lg">
-            What needs to be done?
-          </label>
-        </h2>
+      <Form onSubmit={addTask} />
 
-        <input
-          type="text"
-          id="new-todo-input"
-          className="input input__lg"
-          name="text"
-          autoComplete="off"
-        />
+      <div className="filters btn-group stack-exception">
+        <FilterButton />
+        <FilterButton />
+        <FilterButton />
+      </div>
 
-        <button type="submit" className="btn btn__primary btn__lg">
-          Add
-        </button>
-      </form>
+      <h2 id="list-heading">
+        3 tasks remaining
+      </h2>
+
+      <ul
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading"
+      >
+        {taskList}
+      </ul>
     </div>
   );
 }
