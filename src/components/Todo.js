9ebf6f8c2d42373c4a69a6277af1d7172561055c
Arthur Freitas <arthur.freitas@compasso.com.br>
2021-03-08 11:09:37 -0300

    fix(todo): set focus when editing a task

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,8 +1,21 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+
+function usePrevious(value) {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
 
 export default function Todo(props) {
   const [isEditing, setEditing] = useState(false);
   const [newName, setNewName] = useState('');
+
+  const editFieldRef = useRef(null);
+  const editButtonRef = useRef(null);
+
+  const wasEditing = usePrevious(isEditing);
 
   function handleChange(e) {
     setNewName(e.target.value);
@@ -37,6 +50,7 @@ export default function Todo(props) {
           type="button"
           className="btn"
           onClick={() => setEditing(true)}
+          ref={editButtonRef}
         >
           Edit <span className="visually-hidden">{props.name}</span>
         </button>
@@ -63,6 +77,7 @@ export default function Todo(props) {
           type="text"
           value={newName}
           onChange={handleChange}
+          ref={editFieldRef}
         />
       </div>
 
@@ -70,6 +85,7 @@ export default function Todo(props) {
         <button
           type="button"
           className="btn todo-cancel"
+          onClick={() => setEditing(false)}
         >
           Cancel
           <span className="visually-hidden">renaming {props.name}</span>
@@ -84,7 +100,18 @@ export default function Todo(props) {
         </button>
       </div>
     </form>
-  )
+  );
+
+
+  useEffect(() => {
+    if (!wasEditing && isEditing) {
+      editFieldRef.current.focus();
+    }
+    if (wasEditing && !isEditing) {
+      editButtonRef.current.focus();
+    }
+  }, [wasEditing, isEditing]);
+  
 
   return (
     <li className="todo stack-small">
